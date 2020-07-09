f7e2d440243a6954f85741d1eeba5cc006c55d0a
LE ROCH <timothy.leroch@precom-obseques.fr>
2020-07-09 15:09:07 +0200

    change conf

---
+++ conf/local/db.js
@@ -0,0 +1,27 @@
+var DBCONF;
+
+if(process.env.NODE_ENV === "production"){
+  DBCONF = {
+    host: 'eu-cdbr-west-03.cleardb.net',
+    // port: 3306,
+    db: 'heroku_69779c33911c09f',
+    user: 'b9aa848bfe6323',
+    password: '2e92cf4b',
+    dialect: 'mysql', // mariadb, mysql
+    // DEV - display MySQL logs in stdout when true
+    inspect: false
+  }
+} else {
+  DBCONF = {
+    host: 'localhost',
+    // port: 3306,
+    db: 'hyip',
+    user: 'newuser',
+    password: 'password',
+    dialect: 'mysql', // mariadb, mysql
+    // DEV - display MySQL logs in stdout when true
+    inspect: false
+  }
+}
+
+module.exports = DBCONF;
