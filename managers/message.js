3c8d7e6c6dea022fc1357473e3b9a754ffe4a6c7
LE ROCH <timothy.leroch@precom-obseques.fr>
2020-07-21 17:13:18 +0200

    add model Message

--- managers/message.js
+++ managers/message.js
@@ -1,7 +1,6 @@
 const { Message } = require('../models')
 
 const create = function(body){
-  console.log('body received', body)
   return Message.create({
     name: body.name,
     mail: body.mail,
